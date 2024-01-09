d833132c6a389d5e34c1ac311513b47422bcc3b7
vmonney <45258650+vmonney@users.noreply.github.com>
2024-01-09 16:02:04 +0100

    completed

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ chain_type_kwargs = {"prompt": PROMPT}
 llm = CTransformers(
     model="models/llama-2-7b-chat.ggmlv3.q4_0.bin",
     model_type="llama",
-    config={"max_new_tokens": 1024, "temperature": 0.8},
+    config={"max_new_tokens": 512, "temperature": 0.8},
 )
 
 qa = RetrievalQA.from_chain_type(
@@ -56,5 +56,15 @@ def index() -> str:
     return render_template("chat.html")
 
 
+@app.route("/get", methods=["GET", "POST"])
+def chat() -> str:
+    """Chatbot response."""
+    msg = request.form["msg"]
+    print(msg)
+    result = qa({"query": msg})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(debug=True)
